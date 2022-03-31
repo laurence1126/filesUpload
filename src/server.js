a2bb841ca94aeaab7edaf8d79611180cce29e138
laurence1126 <wadewilson@foxmail.com>
2022-03-31 18:42:25 +0800

    3.31

--- src/server.js
+++ src/server.js
@@ -10,8 +10,9 @@ const waitUntil = require('wait-until');
 const emitter = new events.EventEmitter();
 
 
-const homeDir = '/Users/laurence/Documents/VS Code/Node.js/NodeProj/';
+const homeDir = '/Users/laurence/Documents/VS Code/Node.js/FilesUpload/';
 var currentDirectory = homeDir;
+var prevDirectory = [];
 const showHidden = false;
 const videoList = ['.mp4'];
 const imageList = ['.png', ',jpeg', '.jpg']
@@ -33,71 +34,56 @@ app.locals = {
 }
  
 
-var getInfo;
 function getDirInfo (currentDir){
     var fileList = [];
-    fs.readdir(currentDir, function(err, files){
-        if (err) {
-            return console.error(err);
-        }
-        files.forEach(function(file){
-            if (!showHidden){
-                if (file[0] !== "."){
-                    fileList.push(file);
-                }
-            } else {
+    files = fs.readdirSync(currentDir)
+    files.forEach(function(file){
+        if (!showHidden){
+            if (file[0] !== "."){
                 fileList.push(file);
             }
-        });
-        emitter.emit('scan finished'); 
+        } else {
+            fileList.push(file);
+        }
     });
-    emitter.once('scan finished', function(){
-        result = [];
-        result.push(currentDir);
-        result.push(fileList);
-        getInfo = result;
-        emitter.emit('Success');
-    });
+    result = [];
+    result.push(currentDir);
+    result.push(fileList);
+    return result;
 }
 
 
 
 app.get('/', function (req, res){
-    fs.stat(currentDirectory, function(err, stats){
-        if (err){
-            currentDirectory = homeDir;
+    currentDirectory = homeDir;
+    getInfo = getDirInfo(currentDirectory);
+    var items = "";
+    var count = 1;
+    for (var i = 0; i < getInfo[1].length; i++){
+        var tmp = getInfo[1][i];
+        var stats = fs.statSync(currentDirectory + tmp)
+        if (tmp.length > 15){
+            displayName = tmp.substring(0, 15) + '...';
+        } else {
+            displayName = tmp;
         }
-        getDirInfo(currentDirectory);
-        emitter.once("Success", function(){
-            var items = "";
-            var count = 1;
-            for (var i = 0; i < getInfo[1].length; i++){
-                var tmp = getInfo[1][i];
-                var stats = fs.statSync(currentDirectory + tmp)
-                if (tmp.length > 15){
-                    displayName = tmp.substring(0, 15) + '...';
-                } else {
-                    displayName = tmp;
-                }
-                if (stats.isDirectory()){
-                    items += "<li><a class='text-decoration-none' href='#' id='"+count+"th-file'><div class='file-item'><img src='/images/folder.png'><p>" + displayName + "</p><p style='display: none'>" + tmp + "</p></div></a></li>";
-                    count++;
-                } else {
-                    if (videoList.includes(path.extname(currentDirectory + tmp))){
-                        items += "<li><a class='text-decoration-none' href='#' id='"+count+"th-file'><div class='file-item'><img src='/images/video.png'><p>" + displayName + "</p><p style='display: none'>" + tmp + "</p></div></a></li>";
-                        count++;
-                    } else {
-                        items += "<li><a class='text-decoration-none' href='#' id='"+count+"th-file'><div class='file-item'><img src='/images/file.png'><p>"  + displayName + "</p><p style='display: none'>" + tmp + "</p></div></a></li>";
-                        count++;
-                    }
-                }
+        if (stats.isDirectory()){
+            items += "<li><a class='text-decoration-none' href='#' id='"+count+"th-file'><div class='file-item'><img src='/images/folder.png'><p>" + displayName + "</p><p style='display: none'>" + tmp + "</p></div></a></li>";
+            count++;
+        } else {
+            if (videoList.includes(path.extname(currentDirectory + tmp))){
+                items += "<li><a class='text-decoration-none' href='#' id='"+count+"th-file'><div class='file-item'><img src='/images/video.png'><p>" + displayName + "</p><p style='display: none'>" + tmp + "</p></div></a></li>";
+                count++;
+            } else {
+                items += "<li><a class='text-decoration-none' href='#' id='"+count+"th-file'><div class='file-item'><img src='/images/file.png'><p>"  + displayName + "</p><p style='display: none'>" + tmp + "</p></div></a></li>";
+                count++;
             }
-            res.render('index', {title: "Upload Files",
-                currentDirectory: "<h2>" + getInfo[0] + "</h2>",
-                filelist: items
-            });
-        })
-    })
+        }
+    }
+    res.render('index', {title: "Upload Files",
+        currentDirectory: "<h2>" + getInfo[0] + "</h2>",
+        filelist: items
+    });
 })
 
 app.get('/resume', function (req, res){
@@ -116,9 +102,16 @@ app.post('/changeDir', urlencodedParser, function(req, res){
     var response = {
         "request_dir": req.body.requestDir,
     };
-    if (req.body.requestDir === "home"){
+    if (response.request_dir === "home"){
         currentDirectory = homeDir.substring(0, homeDir.length-1);
+    } else if (response.request_dir === "forward"){
+        if (prevDirectory.length > 0){
+            currentDirectory = prevDirectory.pop();
+        }
     } else {
+        if (response.request_dir !== './'){
+            prevDirectory.push(currentDirectory);
+        }
         len = currentDirectory.split('/').length;
         while (currentDirectory.split('/')[len-1] === ""){
             len -= 1;
@@ -126,10 +119,8 @@ app.post('/changeDir', urlencodedParser, function(req, res){
         if (currentDirectory.split('/')[len-1] !== response.request_dir){
             currentDirectory +=  response.request_dir;
         }
-
-        currentDirectory = path.resolve(currentDirectory);
     }
-    
+    currentDirectory = path.resolve(currentDirectory);
     fs.stat(currentDirectory, function(err, stats){
         if (err){
             currentDirectory = homeDir;
@@ -139,96 +130,91 @@ app.post('/changeDir', urlencodedParser, function(req, res){
             if (stats.isDirectory()){
                 currentDirectory += '/';
                 console.log('Visiting: ' + currentDirectory);
-                getDirInfo(currentDirectory);
-                emitter.once("Success", function(){
-                    var items = "";
-                    var count = 1;
-                    for (var i = 0; i < getInfo[1].length; i++){
-                        var tmp = getInfo[1][i];
-                        var stats = fs.statSync(currentDirectory + tmp);
-                        if (tmp.length > 15){
-                            displayName = tmp.substring(0, 15) + '...'
-                        } else {
-                            displayName = tmp;
-                        }
-                        if (stats.isDirectory()){
-                            items += "<li><a class='text-decoration-none' href='#' id='"+count+"th-file'><div class='file-item'><img src='/images/folder.png'><p>" + displayName + "</p><p style='display: none'>" + tmp + "</p></div></a></li>";
+                getInfo = getDirInfo(currentDirectory);
+                var items = "";
+                var count = 1;
+                for (var i = 0; i < getInfo[1].length; i++){
+                    var tmp = getInfo[1][i];
+                    var stats = fs.statSync(currentDirectory + tmp);
+                    if (tmp.length > 15){
+                        displayName = tmp.substring(0, 15) + '...'
+                    } else {
+                        displayName = tmp;
+                    }
+                    if (stats.isDirectory()){
+                        items += "<li><a class='text-decoration-none' href='#' id='"+count+"th-file'><div class='file-item'><img src='/images/folder.png'><p>" + displayName + "</p><p style='display: none'>" + tmp + "</p></div></a></li>";
+                        count++;
+                    } else {
+                        if (videoList.includes(path.extname(currentDirectory + tmp))){
+                            items += "<li><a class='text-decoration-none' href='#' id='"+count+"th-file'><div class='file-item'><img src='/images/video.png'><p>" + displayName + "</p><p style='display: none'>" + tmp + "</p></div></a></li>";
                             count++;
                         } else {
-                            if (videoList.includes(path.extname(currentDirectory + tmp))){
-                                items += "<li><a class='text-decoration-none' href='#' id='"+count+"th-file'><div class='file-item'><img src='/images/video.png'><p>" + displayName + "</p><p style='display: none'>" + tmp + "</p></div></a></li>";
-                                count++;
-                            } else {
-                                items += "<li><a class='text-decoration-none' href='#' id='"+count+"th-file'><div class='file-item'><img src='/images/file.png'><p>" + displayName + "</p><p style='display: none'>" + tmp + "</p></div></a></li>";
-                                count++;
-                            }
+                            items += "<li><a class='text-decoration-none' href='#' id='"+count+"th-file'><div class='file-item'><img src='/images/file.png'><p>" + displayName + "</p><p style='display: none'>" + tmp + "</p></div></a></li>";
+                            count++;
                         }
                     }
-                    res.render('index', {title: "Upload Files",
-                        currentDirectory: "<h2>" + getInfo[0] + "</h2>",
-                        filelist: items
-                    });
-                })
+                }
+                res.render('index', {title: "Upload Files",
+                    currentDirectory: "<h2>" + getInfo[0] + "</h2>",
+                    filelist: items
+                });
             } else{
                 console.log('Trying to fetch file: ' + currentDirectory)
                 currentDirectory = path.dirname(currentDirectory) + '/';
-                getDirInfo(currentDirectory);
-                emitter.once("Success", function(){
-                    var items = "";
-                    var count = 1;
-                    for (var i = 0; i < getInfo[1].length; i++){
-                        var tmp = getInfo[1][i];
-                        var stats = fs.statSync(currentDirectory + tmp);
-                        if (tmp.length > 15){
-                            displayName = tmp.substring(0, 15) + '...'
-                        } else {
-                            displayName = tmp;
-                        }
-                        if (stats.isDirectory()){
-                            items += "<li><a class='text-decoration-none' href='#' id='"+count+"th-file'><div class='file-item'><img src='/images/folder.png'><p>" + displayName + "</p><p style='display: none'>" + tmp + "</p></div></a></li>";
+                getInfo = getDirInfo(currentDirectory);
+                var items = "";
+                var count = 1;
+                for (var i = 0; i < getInfo[1].length; i++){
+                    var tmp = getInfo[1][i];
+                    var stats = fs.statSync(currentDirectory + tmp);
+                    if (tmp.length > 15){
+                        displayName = tmp.substring(0, 15) + '...'
+                    } else {
+                        displayName = tmp;
+                    }
+                    if (stats.isDirectory()){
+                        items += "<li><a class='text-decoration-none' href='#' id='"+count+"th-file'><div class='file-item'><img src='/images/folder.png'><p>" + displayName + "</p><p style='display: none'>" + tmp + "</p></div></a></li>";
+                        count++;
+                    } else {
+                        if (videoList.includes(path.extname(currentDirectory + tmp))){
+                            items += "<li><a class='text-decoration-none' href='#' id='"+count+"th-file'><div class='file-item'><img src='/images/video.png'><p>" + displayName + "</p><p style='display: none'>" + tmp + "</p></div></a></li>";
                             count++;
                         } else {
-                            if (videoList.includes(path.extname(currentDirectory + tmp))){
-                                items += "<li><a class='text-decoration-none' href='#' id='"+count+"th-file'><div class='file-item'><img src='/images/video.png'><p>" + displayName + "</p><p style='display: none'>" + tmp + "</p></div></a></li>";
-                                count++;
-                            } else {
-                                items += "<li><a class='text-decoration-none' href='#' id='"+count+"th-file'><div class='file-item'><img src='/images/file.png'><p>" + displayName + "</p><p style='display: none'>" + tmp + "</p></div></a></li>";
-                                count++;
-                            }
+                            items += "<li><a class='text-decoration-none' href='#' id='"+count+"th-file'><div class='file-item'><img src='/images/file.png'><p>" + displayName + "</p><p style='display: none'>" + tmp + "</p></div></a></li>";
+                            count++;
                         }
                     }
-                    res.render('index', {title: "Upload Files",
-                        currentDirectory: "<h2>" + getInfo[0] + "</h2>",
-                        filelist: items,
-                        content: `
-                            <script>
-                                $.sendConfirm({
-                                    title: 'Information',
-                                    withNoMinWidth: true,
-                                    width: '400px',
-                                    content: '<h4 style="margin: 0; margin-bottom: 30px">Do you want to download this file?</h4>',
-                                    button: {
-                                        confirm: 'Download',
-                                        cancel: 'Cancel'
-                                    },
-                                    onConfirm: function() {
-                                        $("#download").submit();
-                                    },
-                                    onCancel: function() {
-                                        console.log('点击取消！');
-                                    },
-                                    onClose: function() {
-                                        console.log('点击关闭！');
-                                    }
-                                });
-                            </script>`,
-                        downloadItem: response.request_dir
-                    });
-                })
+                }
+                res.render('index', {title: "Upload Files",
+                    currentDirectory: "<h2>" + getInfo[0] + "</h2>",
+                    filelist: items,
+                    content: `
+                        <script>
+                            $.sendConfirm({
+                                title: 'Information',
+                                withNoMinWidth: true,
+                                width: '400px',
+                                content: '<h4 style="margin: 0; margin-bottom: 30px">Do you want to download this file?</h4>',
+                                button: {
+                                    confirm: 'Download',
+                                    cancel: 'Cancel'
+                                },
+                                onConfirm: function() {
+                                    $("#download").submit();
+                                },
+                                onCancel: function() {
+                                    console.log('点击取消！');
+                                },
+                                onClose: function() {
+                                    console.log('点击关闭！');
+                                }
+                            });
+                        </script>`,
+                    downloadItem: response.request_dir
+                });
             }
         }
     })
-    
 })
 
 app.post('/download', urlencodedParser, function(req, res){
